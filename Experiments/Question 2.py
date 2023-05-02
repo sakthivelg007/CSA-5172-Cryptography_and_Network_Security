fa82ff55f55cd0f0e7ee5d0fcaee92612e4dad85
Sakthivel G <100275995+sakthivelg007@users.noreply.github.com>
2023-05-02 10:04:29 +0530

    Create Question 2.py
    Python program for monoalphabetic substitution cipher maps a plaintext alphabet to a ciphertext
    alphabet, so that each letter of the plaintext alphabet maps to a single unique letter of the ciphertext
    alphabet.

---
+++ Experiments/Question 2.py
@@ -0,0 +1,27 @@
+plaintext = input("Enter plaintext to encrypt: ")
+key = "XPMGTDHLYONZBWEARKJUFSCIQV"
+ciphertext = ""
+
+for char in plaintext:
+    if char.isalpha():
+        if char.isupper():
+            ciphertext += key[ord(char) - ord('A')].upper()
+        else:
+            ciphertext += key[ord(char) - ord('a')].lower()
+    else:
+        ciphertext += char
+
+print("Ciphertext:", ciphertext)
+
+decrypted_plaintext = ""
+
+for char in ciphertext:
+    if char.isalpha():
+        if char.isupper():
+            decrypted_plaintext += chr(key.upper().index(char) + ord('A'))
+        else:
+            decrypted_plaintext += chr(key.lower().index(char) + ord('a'))
+    else:
+        decrypted_plaintext += char
+
+print("Decrypted plaintext:", decrypted_plaintext)
